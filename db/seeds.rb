0974f68cdf1812a2ac6ab103b450e94c3436f46a
Chris Moore <chris@knowndecimal.com>
2021-01-04 22:37:05 -0500

    Seed some tasks for fun

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create!(title: 'task 10m', estimate: 10)
+Task.create!(title: 'task 20m', estimate: 20)
+Task.create!(title: 'task 30m', estimate: 30)
+Task.create!(title: 'task 1h', estimate: 60)
