9d8d86ddd7d8f8a5b9ebca9c8482c1d9a3304dc6
Chris Moore <chris@knowndecimal.com>
2021-01-04 21:16:41 -0500

    Rails handles edit, JS listens for dblclick
    The JS controller's only role here is to listen for the double click
    event, then navigate to the edit link.

--- app/javascript/controllers/task_controller.js
+++ app/javascript/controllers/task_controller.js
@@ -1,39 +1,10 @@
 import { Controller } from "stimulus"
-import {Turbo} from "@hotwired/turbo-rails"
 
 export default class extends Controller {
-  static values = {id: String, editUrl: String};
   static targets = ["editLink"];
   
-  constructor(context) {
-    super(context);
-  }
-  
-  subscribe() {
-    return [
-      tasks.subscribe(() => this.update())
-    ]
-  }
-  
-  handleClick(ev) {
-    if (ev.target != this.editLinkTarget) {
-      ev.stopPropagation();  
-    }
-    
-    tasks.setState({selectedId: this.idValue});
-    this.update();
-  }
-  
-  handleDoubleClick() {
-    // console.log("edit link:", this.editLinkTarget);
-    this.editLinkTarget.click();
-    // const url = this.editUrlValue;
-    // Turbo.visit(url);
-  }
-  
-  update() {
-    const selectedId = tasks.getState().selectedId;
-    this.element.classList.toggle("selected", selectedId == this.idValue);
+  edit() {
+    this.editLinkTarget.click()
   }
 }
 
